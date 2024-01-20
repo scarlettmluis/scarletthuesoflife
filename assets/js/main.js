6403df4712e2c8e2f84b3838c8a6835af0ee770f
Katie Chavez <kchavez288@gmail.com>
2024-01-20 11:46:07 -0500

    view more
    view more on testimonial added. Need to finish blog categories

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,6 +123,22 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
     }
 });
 
+function myFunctionn() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
